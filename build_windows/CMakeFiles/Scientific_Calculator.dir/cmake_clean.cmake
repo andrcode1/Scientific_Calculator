fbd730d0784ee555d41a39455430d555cfd01af8
andrcode1 <alexdidko6@gmail.com>
2025-03-15 14:47:17 +0100

    Renamed build to build_windows

---
+++ build_windows/CMakeFiles/Scientific_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scientific_Calculator_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Scientific_Calculator_autogen.dir/ParseCache.txt"
+  "Scientific_Calculator_autogen"
+  "CMakeFiles/Scientific_Calculator.dir/Scientific_Calculator_autogen/EWIEGA46WW/qrc_images.cpp.obj"
+  "CMakeFiles/Scientific_Calculator.dir/Scientific_Calculator_autogen/EWIEGA46WW/qrc_images.cpp.obj.d"
+  "CMakeFiles/Scientific_Calculator.dir/Scientific_Calculator_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/Scientific_Calculator.dir/Scientific_Calculator_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/Scientific_Calculator.dir/Source_Files/CalculateExpression.cpp.obj"
+  "CMakeFiles/Scientific_Calculator.dir/Source_Files/CalculateExpression.cpp.obj.d"
+  "CMakeFiles/Scientific_Calculator.dir/Source_Files/main.cpp.obj"
+  "CMakeFiles/Scientific_Calculator.dir/Source_Files/main.cpp.obj.d"
+  "CMakeFiles/Scientific_Calculator.dir/Source_Files/mainwindow.cpp.obj"
+  "CMakeFiles/Scientific_Calculator.dir/Source_Files/mainwindow.cpp.obj.d"
+  "Scientific_Calculator.exe"
+  "Scientific_Calculator.exe.manifest"
+  "Scientific_Calculator.pdb"
+  "Scientific_Calculator_autogen/EWIEGA46WW/qrc_images.cpp"
+  "Scientific_Calculator_autogen/mocs_compilation.cpp"
+  "Scientific_Calculator_autogen/timestamp"
+  "libScientific_Calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Scientific_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_windows/CMakeFiles/Scientific_Calculator_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for Scientific_Calculator_autogen.
+# This may be replaced when dependencies are built.
