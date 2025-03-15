8cc48a46c585dd71106294d8bee378acdfbd51fd
Andrei <alexdidko6@gmail.com>
2025-03-15 14:48:10 +0100

    Delete old build directory

--- build/CMakeFiles/Scientific_Calculator.dir/Scientific_Calculator_autogen/EWIEGA46WW/qrc_images.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Scientific_Calculator.dir/Scientific_Calculator_autogen/EWIEGA46WW/qrc_images.cpp.obj: \
- C:\Users\Andre\Desktop\uni\Scientific_Calculator\build\Scientific_Calculator_autogen\EWIEGA46WW\qrc_images.cpp
--- build/CMakeFiles/Scientific_Calculator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Scientific_Calculator.
-# This may be replaced when dependencies are built.
